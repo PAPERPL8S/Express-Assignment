b99443f05d88fff70f96caf43097128b7f73ec71
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-11-03 19:30:18 -0600

    simplified code

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 // all your code should go below this line
+
 import express from "express";
 import { prisma } from "../prisma/prisma-instance";
 import { errorHandleMiddleware } from "./error-handler";
 import "express-async-errors";
-import {
-  body,
-  validationResult,
-  ValidationError,
-} from "express-validator";
+
+//get all dog data
 
 const app = express();
 app.use(express.json());
@@ -16,7 +14,6 @@ app.get("/", (_req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
-// GET /dogs - returns all dogs
 app.get("/dogs", async (_req, res) => {
   try {
     const dogs = await prisma.dog.findMany();
@@ -28,23 +25,24 @@ app.get("/dogs", async (_req, res) => {
   }
 });
 
-// GET /dogs/:id - returns a single dog by id
 app.get("/dogs/:id", async (req, res) => {
-  const { id } = req.params;
+  const id = Number(req.params.id);
 
-  if (isNaN(Number(id))) {
-    return res.status(400).json({
-      message: "id should be a number",
-    });
+  if (isNaN(id)) {
+    return res
+      .status(400)
+      .json({ message: "id should be a number" });
   }
 
   try {
     const dog = await prisma.dog.findUnique({
-      where: { id: Number(id) },
+      where: { id },
     });
 
     if (!dog) {
-      return res.status(204).send();
+      return res
+        .status(204)
+        .json({ message: "Dog not found" });
     }
 
     return res.status(200).json(dog);
@@ -56,170 +54,116 @@ app.get("/dogs/:id", async (req, res) => {
 });
 
 // POST /dogs - creates a new dog
-app.post(
-  "/dogs",
-  [
-    body("name")
-      .isString()
-      .withMessage("name should be a string"),
-    body("description")
-      .isString()
-      .withMessage("description should be a string"),
-    body("age")
-      .optional()
-      .isNumeric()
-      .withMessage("age should be a number"),
-    body("breed")
-      .optional()
-      .isString()
-      .withMessage("breed should be a string"),
-  ],
-  async (req: express.Request, res: express.Response) => {
-    const errors = validationResult(req);
+app.post("/dogs", async (req, res) => {
+  const errors: string[] = [];
+  const allowedKeys = [
+    "name",
+    "description",
+    "breed",
+    "age",
+  ];
 
-    function isFieldError(
-      error: ValidationError
-    ): error is ValidationError & { param: string } {
-      return "param" in error;
-    }
+  const { name, description, breed, age } = req.body;
 
-    if (!errors.isEmpty()) {
-      const formattedErrors = errors
-        .array()
-        .map((error) => ({
-          message: error.msg,
-          field: isFieldError(error)
-            ? error.param
-            : "unknown",
-        }));
+  if (typeof age != "number") {
+    errors.push("age should be a number");
+  }
 
-      return res.status(400).json({
-        errors: formattedErrors.map((e) => e.message),
-      });
-    }
+  if (typeof name !== "string") {
+    errors.push("name should be a string");
+  }
 
-    const allowedKeys = [
-      "name",
-      "description",
-      "age",
-      "breed",
-    ];
-    const receivedKeys = Object.keys(req.body);
-    const invalidKeys = receivedKeys.filter(
-      (key) => !allowedKeys.includes(key)
-    );
+  if (typeof description !== "string") {
+    errors.push("description should be a string");
+  }
 
-    if (invalidKeys.length > 0) {
-      const invalidKeyErrors = invalidKeys.map((key) => ({
-        message: `'${key}' is not a valid key`,
-        field: key,
-      }));
-      return res.status(400).json({
-        errors: invalidKeyErrors.map((e) => e.message),
-      });
-    }
+  if (typeof breed !== "string") {
+    errors.push("breed should be a string");
+  }
 
-    const { name, description, breed, age } = req.body;
-    try {
-      const newDog = await prisma.dog.create({
-        data: {
-          name,
-          description,
-          breed,
-          age,
-        },
-      });
-      return res.status(201).json(newDog);
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ error: "Failed to create dog" });
+  for (const key in req.body) {
+    if (!allowedKeys.includes(key)) {
+      errors.push(`'${key}' is not a valid key`);
     }
   }
-);
+
+  if (errors.length) {
+    return res.status(400).send({ errors });
+  }
+
+  try {
+    const newDog = await prisma.dog.create({
+      data: {
+        name,
+        description,
+        breed,
+        age,
+      },
+    });
+    return res.status(201).json(newDog);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Failed to create dog" });
+  }
+});
 
 // PATCH /dogs/:id - updates a dog by id
-app.patch(
-  "/dogs/:id",
-  [
-    body("name")
-      .optional()
-      .isString()
-      .withMessage("Name must be a string"),
-    body("description")
-      .optional()
-      .isString()
-      .withMessage("Description must be a string"),
-    body("breed")
-      .optional()
-      .isString()
-      .withMessage("Breed must be a string"),
-    body("age")
-      .optional()
-      .isNumeric()
-      .withMessage("Age must be a number"),
-  ],
-  async (req: express.Request, res: express.Response) => {
-    const { id } = req.params;
-    const errors = validationResult(req);
+app.patch("/dogs/:id", async (req, res) => {
+  const { id } = req.params;
+  const errors: string[] = [];
+  const allowedKeys = [
+    "name",
+    "description",
+    "breed",
+    "age",
+  ];
 
-    function isFieldError(
-      error: ValidationError
-    ): error is ValidationError & { param: string } {
-      return "param" in error;
-    }
+  const { name, description, breed, age } = req.body;
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array().map((error) => ({
-          message: error.msg,
-          field: isFieldError(error)
-            ? error.param
-            : "unknown",
-        })),
-      });
-    }
+  if (age !== undefined && typeof age !== "number") {
+    errors.push("age should be a number");
+  }
+  if (name !== undefined && typeof name !== "string") {
+    errors.push("name should be a string");
+  }
+  if (
+    description !== undefined &&
+    typeof description !== "string"
+  ) {
+    errors.push("description should be a string");
+  }
+  if (breed !== undefined && typeof breed !== "string") {
+    errors.push("breed should be a string");
+  }
 
-    const allowedKeys = [
-      "name",
-      "description",
-      "age",
-      "breed",
-    ];
-    const receivedKeys = Object.keys(req.body);
-    const invalidKeys = receivedKeys.filter(
-      (key) => !allowedKeys.includes(key)
-    );
-
-    if (invalidKeys.length > 0) {
-      const invalidKeyErrors = invalidKeys.map((key) => ({
-        message: `'${key}' is not a valid key`,
-        field: key,
-      }));
-      return res
-        .status(400)
-        .json({ errors: invalidKeyErrors });
-    }
-
-    const { name, description, breed, age } = req.body;
-    try {
-      const updateDog = await prisma.dog.update({
-        where: { id: Number(id) },
-        data: { name, description, breed, age },
-      });
-      res.status(201).json(updateDog);
-    } catch (error) {
-      if ((error as { code?: string }).code === "P2025") {
-        return res
-          .status(404)
-          .json({ error: "Dog not found" });
-      }
-      res
-        .status(500)
-        .json({ error: "Failed to update dog" });
+  for (const key in req.body) {
+    if (!allowedKeys.includes(key)) {
+      errors.push(`'${key}' is not a valid key`);
     }
   }
-);
+
+  if (errors.length) {
+    return res.status(400).json({ errors });
+  }
+
+  try {
+    const updatedDog = await prisma.dog.update({
+      where: { id: Number(id) },
+      data: { name, description, breed, age },
+    });
+    return res.status(201).json(updatedDog);
+  } catch (error) {
+    if ((error as { code?: string }).code === "P2025") {
+      return res
+        .status(404)
+        .json({ error: "Dog not found" });
+    }
+    return res
+      .status(500)
+      .json({ error: "Failed to update dog" });
+  }
+});
 
 // DELETE /dogs/:id - Delete a dog via id
 app.delete("/dogs/:id", async (req, res) => {
